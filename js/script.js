75bfad708075f816c0caade4b05aaaff3eed968a
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-02-17 19:21:50 +0100

    Made base

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector(".container");
+
+async function callApi() {
+  const respons = await fetch(
+    "https://genius.p.rapidapi.com/artists/16775/songs",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "genius.p.rapidapi.com",
+        "x-rapidapi-key": "7a735c65b0msh4b4917a539d36dap135f69jsna87e32d3342f",
+      },
+    }
+  );
+
+  const json = await respons.json();
+  const data = json.response.songs;
+
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].artist_names);
+    console.log(data[i].full_title);
+
+    container.innerHTML += `
+    <div>
+      <h2>Song: ${data[i].full_title}</h2>
+      <h3>Artist: ${data[i].artist_names}</h3>
+    </div>`;
+  }
+
+  console.log(data);
+}
+
+callApi();
